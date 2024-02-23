6bc356cf11614825d269b45909efd6f3034c710a
gukihuman <sokolovivanf@gmail.com>
2024-02-23 07:44:52 +0500

    make plugin that loads webp

--- components/Mailings.vue
+++ components/Mailings.vue
@@ -45,14 +45,11 @@ export type MailingItem = {
     users: number
     image: string
 }
-type WebpPaths = { [fileName: string]: string }
-const webpPaths: WebpPaths = {}
+import { webpPaths } from "@/plugins/loadWebpImages"
+
 const mailingData: Ref<MailingItem[]> = ref([])
 onMounted(async () => {
-    await loadWebpPaths(import.meta.glob("@/assets/**/*"), webpPaths)
-    setMailingData()
-})
-const setMailingData = () => {
+    /** Fill data object after webPaths is loaded on app start */
     mailingData.value = [
         {
             time: "Перед рассветом",
@@ -127,28 +124,5 @@ const setMailingData = () => {
             image: webpPaths["item-4"],
         },
     ]
-}
-
-/** Took an object from import.meta.glob function and creates a WebpPaths
- * object with paths to webp images. import.meta.glob must be used with a direct string path, not variable (that's why it cannot be used inside this function, otherwise path must be incapsulated inside too). */
-async function loadWebpPaths(
-    paths: Record<string, () => Promise<Record<string, any>>>,
-    savePlace: WebpPaths
-) {
-    for (const path in paths) {
-        const name = parseFileName(path)
-        if (!name) continue
-        const item = await paths[path]()
-        savePlace[name] = item.default // it's a string - path to a file
-    }
-}
-
-/** Extract file name from path without extension.
- * "/path/to/file.webp" -> "file" */
-function parseFileName(path: string) {
-    let match: any[] | null = null
-    match = path.match(/\/([^/]+)\.webp/)
-    if (!match) return
-    return `${match[1]}`
-}
+})
 </script>
